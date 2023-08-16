98332ad5b3bd6e4b86d493e91d96913288b11c6d
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-08-16 15:46:42 -0300

    Implemented server

---
+++ src/server/app.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import "express-async-errors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err}`,
+    });
+  }
+);
+
+export { app };
