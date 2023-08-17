f5a55a2b626df3a3edf5a496dcf94373b2c434cf
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2023-08-17 08:47:04 -0300

    Initiated routes

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import "express-async-errors";
+import { routes } from "../routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use(routes);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
